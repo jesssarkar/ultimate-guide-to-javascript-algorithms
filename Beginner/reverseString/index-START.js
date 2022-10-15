46bb22a9ac1fb57e5f9f2636976353fe4cf2471c
Jessica Sarkar <jessicalosee@gmail.com>
2022-10-15 17:35:50 -0500

    reverse string

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,8 +6,42 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+   return text.split('').reverse().join('')
 }
+
+function reverseString(text) {
+    return [...text].reverse().join('')
+ }
+ 
+ function reverseString(text) {
+    let result = ''
+    for (let i = text.length-1; i>=0; i--){
+        result += text[i]
+    }
+    return result
+ }
+ 
+ function reverseString(text) {
+    let result = ''
+    for (let char of text){
+        result = char + result
+    }
+    return result
+ }
+
+
+ function reverseString(text) {
+   if(text===''){
+       return ''
+   } else{
+       return reverseString(text.substr(1) + text[0])
+   }
+ }
+
+
+ function reverseString(text) {
+    return [...text].reduce((acc, char) => char + acc, '')
+  }
 
 
 
