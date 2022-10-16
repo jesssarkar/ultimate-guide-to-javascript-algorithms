b044fee90ab55dce3ca8d9f87f9f8d96a2163309
Jessica Sarkar <jessicalosee@gmail.com>
2022-10-16 17:46:35 -0500

    added pig latin and searchReplace files

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -7,8 +7,35 @@ e.g palindromeChecker('racecar') // will return true
 
 
 function palindromeChecker(text) {
-    v// Code goes here
+    const reversedText = text.toLowerCase().split('').reverse().join('')
+
+    return text === reversedText
 }
+
+
+
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1]
+    })
+    return result
+}
+
+
+
+function palindromeChecker(text) {
+    const textLen = text.length
+
+    for(let i=0; i<textLen/2; i++){
+        if (text[i] !== text[textLen-1-i]){
+            return false
+        }
+    }
+    return true
+}
+
 
 
 
---
+++ Beginner/searchReplace/index-START.js
@@ -0,0 +1,22 @@
+function searchReplace(str, word, newWord) {
+    let regex = new RegExp(word, "gi")
+
+    if (/[A-Z]/.test(word[0])) {
+
+        newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+    }
+
+    return str.replace(regex, newWord)
+}
+
+
+
+function searchReplace(str, word, newWord) {
+    if(word[0] === word[0].toUpperCase()){
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(word, newWord)
+}
+
+
+module.exports = searchReplace
